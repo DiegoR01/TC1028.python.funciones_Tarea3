60a83afd9bd51b9ab4a41ad012d82832bab7366b
DiegoR01 <88850966+DiegoR01@users.noreply.github.com>
2021-08-29 19:09:16 -0500

    Update exercise.py

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,6 +1,13 @@
 
 def main():
     #escribe tu código abajo de esta línea
+    year=float(input("inserta el año:"))
+
+
+if year % 4 == 0 and (year % 100 != 0 or año % 400 == 0):
+    print ("true")
+else: 
+    print ("false")
     pass
 
 if __name__=='__main__':
